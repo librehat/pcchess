dd83699a72092920b4072876077345be474787bc
Symeon Huang <hzwhuang@gmail.com>
2015-06-19 19:50:01 +0100

    take 'general facing' rule into account for general itself

--- core/general.cpp
+++ core/general.cpp
@@ -15,11 +15,52 @@ int general::value() const
     return 8;
 }
 
-void general::gen_moves(const abstract_piece***)
+void general::gen_moves(const abstract_piece*** b)
 {
     avail_moves.push_back(pos + up);
     avail_moves.push_back(pos + down);
-    avail_moves.push_back(pos + left);
-    avail_moves.push_back(pos + right);
+    
+    position pos_left = pos + left;
+    position pos_right = pos + right;
+    
+    /* 
+     * if there is only one (enemy) general on the left,
+     * then our general can't move to the left.
+     * the same applies to right.
+     */
+    bool found_general = false;
+    bool other_piece_between = false;
+    for (int irank = 0; irank <= 9; irank++) {
+        if (b[pos_left.file][irank]) {
+            if (found_general) {
+                other_piece_between = true;
+                break;
+            } else if (b[pos_left.file][irank]->abbr_name() == 'G') {
+                found_general = true;
+            }
+        }
+    }
+    
+    if (other_piece_between || !found_general) {
+        avail_moves.push_back(pos_left);
+    }
+    
+    found_general = false;
+    other_piece_between = false;
+    for (int irank = 0; irank <= 9; irank++) {
+        if (b[pos_right.file][irank]) {
+            if (found_general) {
+                other_piece_between = true;
+                break;
+            } else if (b[pos_right.file][irank]->abbr_name() == 'G') {
+                found_general = true;
+            }
+        }
+    }
+    
+    if (other_piece_between || !found_general) {
+        avail_moves.push_back(pos_right);
+    }
+    
     remove_invalid_moves(3, 5, 7, 9);
 }
