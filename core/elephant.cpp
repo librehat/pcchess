6a85921b9fd5baf2ebcf541dad4e0b1df703e751
Symeon Huang <zhuang@tcd.ie>
2015-06-30 17:43:51 +0100

    fix elephant moving rule

--- core/elephant.cpp
+++ core/elephant.cpp
@@ -7,6 +7,11 @@ const position elephant::up_right = position(2, -2);
 const position elephant::down_left = position(-2, 2);
 const position elephant::down_right = position(2, 2);
 
+const position elephant::up_left_centre = position(-1, -1);
+const position elephant::up_right_centre = position(1, -1);
+const position elephant::down_left_centre = position(-1, 1);
+const position elephant::down_right_centre = position(1, 1);
+
 abstract_piece* elephant::make_copy_with_new_board(board &bd) const
 {
 	return new elephant(*this, bd);
@@ -29,9 +34,17 @@ int elephant::value() const
 
 void elephant::gen_moves()
 {
-    avail_moves.push_back(pos + up_left);
-    avail_moves.push_back(pos + up_right);
-    avail_moves.push_back(pos + down_left);
-    avail_moves.push_back(pos + down_right);
+    if (!m_board[pos + up_left_centre]) {
+        avail_moves.push_back(pos + up_left);
+    }
+    if (!m_board[pos + up_right_centre]) {
+        avail_moves.push_back(pos + up_right);
+    }
+    if (!m_board[pos + down_left_centre]) {
+        avail_moves.push_back(pos + down_left);
+    }
+    if (!m_board[pos + down_right_centre]) {
+        avail_moves.push_back(pos + down_right);
+    }
     remove_invalid_moves(0, 8, m_opposite ? 0 : 5, m_opposite ? 4 : 9);//elephant can't across the river
 }
--- core/elephant.h
+++ core/elephant.h
@@ -27,6 +27,11 @@ private:
     static const position up_right;
     static const position down_left;
     static const position down_right;
+
+    static const position up_left_centre;
+    static const position up_right_centre;
+    static const position down_left_centre;
+    static const position down_right_centre;
 };
 
 #endif //ELEPHANT_H
