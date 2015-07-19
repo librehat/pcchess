c3a40150a7f7cf734372a710d5d6fab53a381117
Symeon Huang <hzwhuang@gmail.com>
2015-07-19 16:51:59 +0100

    Revert "no need to write that by hand"
    This reverts commit 60c311471109d91c09dd520c808925ef21874f7a.

--- core/position.h
+++ core/position.h
@@ -3,7 +3,7 @@
 
 #include <array>
 #include <boost/serialization/access.hpp>
-#include <boost/serialization/array.hpp>
+#include <boost/serialization/nvp.hpp>
 #include <boost/mpi/datatype.hpp>
 
 class position
@@ -42,6 +42,12 @@ inline bool operator ==(const position &a, const position &b)
 }
 
 typedef std::array<position, 2> pos_move;//moving a piece from move[0] to move[1]
+template<class Archive>
+void serialize(Archive &ar, pos_move &p, const unsigned int version)
+{
+    ar & BOOST_SERIALIZATION_NVP(p[0]);
+    ar & BOOST_SERIALIZATION_NVP(p[1]);
+}
 
 BOOST_IS_MPI_DATATYPE(position)
 BOOST_IS_MPI_DATATYPE(pos_move)
