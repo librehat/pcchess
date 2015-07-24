ba2068e5ff432aaaa858ab412bd753ecd26bca64
Symeon Huang <hzwhuang@gmail.com>
2015-07-24 19:12:59 +0100

    fix a deadlock and use irecv and sleep_for to reduce CPU usage when there is no work

--- player/slow_tree_uct_player.cpp
+++ player/slow_tree_uct_player.cpp
@@ -61,6 +61,9 @@ bool slow_tree_uct_player::think_next_move(pos_move &_move, const board &, const
             next_sync_point = synced_point + sync_period;
         }
         if (!root->select()) {
+#ifdef _DEBUG
+            cout << "don't have more work to do, sleep......" << endl;
+#endif
             this_thread::sleep_for(think_time - elapsed);//we may need to wait for other nodes
             break;
         }
@@ -104,80 +107,69 @@ bool slow_tree_uct_player::think_next_move(pos_move &_move, const board &, const
 
 void slow_tree_uct_player::opponent_moved(const pos_move &m, const abstract_player &opponent)
 {
-    vector<mpi::request> request_vec;
-    for (int i = 1; i < world_comm.size(); ++i) {
-        request_vec.push_back(world_comm.isend(i, TAG_OPPMOV));
-    }
-    for (int i = 1; i < world_comm.size(); ++i) {
-        request_vec[i - 1].wait();
-        request_vec[i - 1] = world_comm.isend(i, TAG_OPPMOV_DATA, m);
-    }
-
-    /* then we do a similar job as uct_player does */
     if (!root) {
         return;
     }
 
     node *new_root = nullptr;
     auto root_iter = root->find_child(m);
-    for (auto &&rt : request_vec) {
-        rt.wait();
-    }
     if (root_iter != root->child_end()) {
+        for (int i = 1; i < world_comm.size(); ++i) {
+            world_comm.send(i, TAG_OPPMOV);
+        }
+        for (int i = 1; i < world_comm.size(); ++i) {
+            world_comm.send(i, TAG_OPPMOV_DATA, m);
+        }
         new_root = root->release_child(root_iter);
         delete root;
         root = new_root;
     } else {
         delete root;
         root = new node(new random_player(*this), new random_player(opponent), true);
-        for (int i = 1; i < world_comm.size(); ++i) {
-            world_comm.send(i, TAG_BROADCAST_TREE);
-        }
         broadcast_tree();
     }
 }
 
 void slow_tree_uct_player::do_slave_job()
 {
-    int tag;
-    mpi::status status;
-
-    if (!root) {
-        broadcast_tree();
-    }
-
+    mpi::request request = world_comm.irecv(0, mpi::any_tag);
     do {
-        status = world_comm.recv(0, mpi::any_tag);
-
-        switch (status.tag()) {
-        case TAG_SYNC:
-            sync_tree();
-            break;
-        case TAG_OPPMOV:
-            slave_opponent_moved();
-            break;
-        case TAG_CHILD_SELEC:
-            slave_select_child();
-            break;
-        case TAG_COMP_LOOP:
-            slave_compute();
-            break;
-        case TAG_COMP_FINISH:
-#ifdef _DEBUG
-            cout << "[" << world_comm.rank() << "] seems I finished my comp_loop earlier than expected, maybe I have no move to test" << endl;
-#endif
-            break;
-        case TAG_BROADCAST_TREE:
-            broadcast_tree();
-            break;
-        case TAG_ERASE:
-            delete root;
-            root = nullptr;
-            break;
-        case TAG_EXIT:
-            return;
-        default:
-            throw invalid_argument("received an invalid mpi tag in do_slave_job()");
+        boost::optional<mpi::status> req_ret = request.test();
+        if (req_ret) {
+            switch (req_ret.get().tag()) {
+            case TAG_SYNC:
+                sync_tree();
+                break;
+            case TAG_OPPMOV:
+                slave_opponent_moved();
+                break;
+            case TAG_CHILD_SELEC:
+                slave_select_child();
+                break;
+            case TAG_COMP_LOOP:
+                slave_compute();
+                break;
+            case TAG_COMP_FINISH:
+    #ifdef _DEBUG
+                cout << "[" << world_comm.rank() << "] seems I finished my comp_loop earlier than expected, maybe I have no move to test" << endl;
+    #endif
+                break;
+            case TAG_BROADCAST_TREE:
+                broadcast_tree();
+                break;
+            case TAG_ERASE:
+                delete root;
+                root = nullptr;
+                break;
+            case TAG_EXIT:
+                return;
+            default:
+                throw invalid_argument("received an invalid mpi tag in do_slave_job()");
+            }
+            request = world_comm.irecv(0, mpi::any_tag);
+        } else {
+            static milliseconds nap(200);
+            this_thread::sleep_for(nap);
         }
     } while (true);
 }
@@ -227,7 +219,11 @@ void slow_tree_uct_player::slave_compute()
     bool cont_comp = true;
     do {
         if (!root->select()) {
-            break;
+#ifdef _DEBUG
+    cout << "[" << world_comm.rank() << "] have no more simulations, I'm going to take a nap for 100 ms" << endl;
+#endif
+            static milliseconds nap(100);
+            this_thread::sleep_for(nap);
         }
         boost::optional<mpi::status> req_ret = request.test();
         if (req_ret) {
@@ -252,6 +248,16 @@ void slow_tree_uct_player::broadcast_tree()
     cout << "[" << world_comm.rank() << "] broadcast_tree" << endl;
 #endif
 
+    if (world_comm.rank() == 0) {
+        for (int i = 1; i < world_comm.size(); ++i) {
+            world_comm.send(i, TAG_BROADCAST_TREE);
+        }
+    } else {
+        if (root) {
+            delete root;
+            root = nullptr;
+        }
+    }
     mpi::broadcast(world_comm, root, 0);
 }
 
@@ -265,6 +271,10 @@ void slow_tree_uct_player::sync_tree()
     vector<node*> tree_vec;
 
     mpi::all_gather(world_comm, root, tree_vec);
+
+#ifdef _DEBUG
+    cout << "[" << world_comm.rank() << "] gathered, merging now......" << endl;
+#endif
 
     int rank = world_comm.rank();
     for(int i = 0; i < world_size; ++i) {
