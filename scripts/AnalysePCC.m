06656f35a0c6b6e61572d60a7ee79e2134bb6a3b
Symeon Huang <zhuang@tcd.ie>
2015-08-14 11:27:18 +0100

    add analysis script (MATLAB/GNU Octave)

---
+++ scripts/AnalysePCC.m
@@ -0,0 +1,23 @@
+function [simpr, speedup, winrate, avgscore] = AnalysePCC(outputfile)
+% This function will calcuate the average simulations per round,
+% speed-up, winning rate, and average score, using given output file.
+
+data = load(outputfile);
+rounds = length(data);
+simpr = 0;
+blacksims = 0;
+wins = 0;
+avgscore = 0;
+for i = 1 : rounds
+    simpr += data(i, 5) / data(i, 2);
+    blacksims += data(i, 6) / data(i, 2);
+    if (data(i, 3) == 1)
+        wins += 1;
+    end
+    avgscore += data(i, 3);
+end
+simpr /= rounds;
+blacksims /= rounds;
+speedup = simpr / blacksims;
+winrate = wins / rounds;
+avgscore /= rounds;
