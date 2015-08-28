a6de9f3345bd10bd73442b2a4f54e71ce6579542
Zhenwei Huang <zhuang@lonsdale01.cluster>
2015-08-28 15:36:17 +0100

    refine the scripts

--- scripts/AnalysePCCBlack.m
+++ scripts/AnalysePCCBlack.m
@@ -1,9 +1,9 @@
-function [games, speedup, speederr, winrate, winerr] = AnalysePCC(outputfile)
+function [games, speedup, speederr, winrate, winerr] = AnalysePCCBlack(outputfile)
 % this function calculates information below:
 % - how many games presented in the output file
-% - the speedup of red player against the black player
+% - the speedup of black player against the red player
 % - the standard error of the speedup
-% - the win rate of red player
+% - the win rate of black player
 % - the standard error of the win rate
 
 data = load(outputfile);
@@ -14,8 +14,8 @@ wins = zeros(games, 1); % 1: win, 0: lose
 for i = 1 : games
     redsims = data(i, 5) / data(i, 2);
     blacksims = data(i, 6) / data(i, 2);
-    speedups(i) = redsims / blacksims;
-    if (data(i, 3) == 1)
+    speedups(i) = blacksims / redsims;
+    if (data(i, 4) == 1)
         wins(i) = 1;
     end
 end
